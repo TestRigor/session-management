ff284418dc8cb75142b88674ce1fb0c73fa3c572
Ivo Parun Rua <ivo.parun.rua@testrigor.com>
2022-06-17 13:50:03 -0300

    Remove unnecessary credentials

--- publish.sh
+++ publish.sh
@@ -4,11 +4,6 @@ PURPLE='\033[0;35m'
 NC='\033[0m'
 GREEN='\033[0;32m'
 
-printf "${PURPLE}Authenticating ...${KUBECONFIG}${NC}\n"
-az login --service-principal -u "${AZURE_CLIENT_ID}" -p "${AZURE_CLIENT_SECRET}" -t "${AZURE_TENANT_ID}" --output none
-az account set --subscription "${AZURE_SUBSCRIPTION_ID}"
-printf "${GREEN}Done${NC}\n"
-
 printf "${PURPLE}Uploading ...${KUBECONFIG}${NC}\n"
 az storage blob upload --connection-string "${STORAGE_ACCOUNT_CONNECTION_STRING}" -f session-management.js -c '$root' -n session-management.js --overwrite true
 printf "${GREEN}Done${NC}\n"
